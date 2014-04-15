db44d3abf88bf5f8d0ee5c5164d4d0c38d019d00
Stelios Kouremenos <stelioskouremenos@unknown-88-1f-a1-03-d1-fe.lan>
2014-04-15 20:24:11 +0300

    changed

--- js/views/HomeView.js
+++ js/views/HomeView.js
@@ -1,16 +1,19 @@
 app.views.HomeView = Backbone.View.extend({
 
     initialize: function () {
-        
+        //this.model = new app.models.Report();
     },
     
     render: function () {
         this.$el.html(this.template());
 
-    },
+    }//,
 
-    events : {
-        "click a": function(event) {
+    //events : {
+       // "click a": function(event) {
+            //this.model.set({description : event.currentTarget.innerText});
+            //console.log(this.model);
+            /*
             if (app.models.currentReport) {
                 app.models.currentReport.set({description : event.currentTarget.innerText});
             } else {
@@ -18,8 +21,8 @@ app.views.HomeView = Backbone.View.extend({
                 app.models.currentReport = new app.models.Report({description : event.currentTarget.innerText});                
             } 
             
-            console.log(app.models.currentReport);
-        }
-    }
+            */
+        //}
+    //}
 
 });
