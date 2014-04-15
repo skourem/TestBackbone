5ef33ed1001a423bd4dc161a19befbbc6ae5da40
Stelios Kouremenos <stelios.kouremenos@gmail.com>
2014-04-15 13:45:23 +0300

    minor changes to HomeView.js

--- js/views/HomeView.js
+++ js/views/HomeView.js
@@ -1,23 +1,22 @@
 app.views.HomeView = Backbone.View.extend({
 
     initialize: function () {
-        
+         
     },
     
     render: function () {
         this.$el.html(this.template());
-
     },
 
     events : {
-        "click a": function(event) {
+        "click a": function(e) {
+            e.preventDefault();
             if (app.models.currentReport) {
-                app.models.currentReport.set({description : event.currentTarget.innerText});
+                app.models.currentReport.set({description : e.currentTarget.innerText});
             } else {
                 //app.models.currentReport = new app.models.Report({description : event.currentTarget.innerText});
-                app.models.currentReport = new app.models.Report({description : event.currentTarget.innerText});                
+                app.models.currentReport = new app.models.Report({description : e.currentTarget.innerText});                
             } 
-            
             console.log(app.models.currentReport);
         }
     }
